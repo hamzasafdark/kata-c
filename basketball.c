d0c3bc7b472f18709cba0b9b33bebafbffbd5a65
Hamza Khan <hamzasadark@gmail.com>
2024-01-16 17:20:11 +0500

    score prompt game

--- basketball.c
+++ basketball.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	int games[4][5] = {{}};
-	double average[4] = {};
+	int games[4][5] = {{},{},{},{}};
+	double average[5] = {};
 	int totalForPlayer = 0;
 	int score;
 	double highest = 0;
@@ -16,22 +16,24 @@ int main() {
 		}
 	}
 
-	for (int k = 0; k < 4;k++) {
-		for (int l = 0; l < 5; l++) {	
-			totalForPlayer += games[l][k];
-		}
-		average[k] = totalForPlayer / 4.00;
-	}
+	for (int k = 0; k < 4; k++) {
+		for (int l = 0; l < 5; l++) {
+			printf("%d ", games[k][l]);	
+			totalForPlayer += games[k][l];
+			average[l] = totalForPlayer / 4.00; 
+		} 
 
+		totalForPlayer = 0;
+	}
 	
-	for (int m = 0; m < 4; m++) {
-		printf("%.2f \n", average[m]);
+	for (int m = 0; m < 5; m++) {
+		printf("\n\nPlayer#%d: %.2f points per game\n", m+1, average[m]);
 		if (average[m] > highest) {
 			highest = average[m];
 		}
 	}
 
-	for (int n=0; n< 4; n++) {
+	for (int n=0; n< 5; n++) {
 		if (highest == average[n]) {
 			printf("Player#%d has the highest scoring average at %.2f points per game\n", n+1, average[n]);
 		}
