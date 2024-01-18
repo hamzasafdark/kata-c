32d63bc363ca4be8ebd5616d6856c9eaec049319
Hamza Khan <hamzasafdark@gmail.com>
2024-01-18 22:49:44 +0500

    check host ip address

--- code/socket.c
+++ code/socket.c
@@ -1,39 +1,65 @@
+/*
+** showip.c -- show IP addresses for a host given on the command line
+*/
+
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
-struct addrinfo
+int main(int argc, char *argv[])
 {
-	int ai_flags;
-	int ai_family;
-	int ai_socktype;
-	int ai_protocol;
-	size_t ai_addrelen;
-	struct sockaddr *ai_addr;
-	char *ai_canonname;
+	struct addrinfo hints, *res, *p;
+	int status;
+	char ipstr[INET6_ADDRSTRLEN];
 
-	strucut addrinfo *ai_next;
-};
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: showip hostname\n");
+		return 1;
+	}
 
-struct sockaddr
-{
-	unsigned short sa_family;
-	char sa_data[14];
-};
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+	hints.ai_socktype = SOCK_STREAM;
 
-struct sockaddr_in
-{
-	short int sin_family;
-	unsigned short int sin_port;
-	struct in_addr sin_addr;
-	unsigned char sin_zero[8];
-};
+	if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return 2;
+	}
 
-struct in_addr
-{
-	uint32_t s_addr;
-};
+	printf("IP addresses for %s:\n\n", argv[1]);
 
-int main()
-{
+	for (p = res; p != NULL; p = p->ai_next)
+	{
+		void *addr;
+		char *ipver;
+
+		// get the pointer to the address itself,
+		// different fields in IPv4 and IPv6:
+		if (p->ai_family == AF_INET)
+		{ // IPv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		}
+		else
+		{ // IPv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		// convert the IP to a string and print it:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf("  %s: %s\n", ipver, ipstr);
+	}
+
+	freeaddrinfo(res); // free the linked list
+
+	return 0;
 }
