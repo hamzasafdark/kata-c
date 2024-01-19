4a61d12cfd8eb22ae2ee7ef471c20cf5e634756b
Hamza Khan <hamzasafdark@gmail.com>
2024-01-19 22:54:04 +0500

    socket programming - backend webserver

--- code/socket.c
+++ code/socket.c
@@ -1,65 +1,164 @@
-/*
-** showip.c -- show IP addresses for a host given on the command line
-*/
-
 #include <stdio.h>
+
+#include <stdlib.h>
+
 #include <string.h>
-#include <sys/types.h>
+
+#include <unistd.h>
+
 #include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+
 #include <netinet/in.h>
 
-int main(int argc, char *argv[])
-{
-	struct addrinfo hints, *res, *p;
-	int status;
-	char ipstr[INET6_ADDRSTRLEN];
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "usage: showip hostname\n");
-		return 1;
-	}
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
-	hints.ai_socktype = SOCK_STREAM;
+//maxium application buffer
 
-	if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
-	{
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-		return 2;
-	}
+#define APP_MAX_BUFFER 1024
 
-	printf("IP addresses for %s:\n\n", argv[1]);
+#define PORT 8080
 
-	for (p = res; p != NULL; p = p->ai_next)
-	{
-		void *addr;
-		char *ipver;
 
-		// get the pointer to the address itself,
-		// different fields in IPv4 and IPv6:
-		if (p->ai_family == AF_INET)
-		{ // IPv4
-			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-			addr = &(ipv4->sin_addr);
-			ipver = "IPv4";
-		}
-		else
-		{ // IPv6
-			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-			addr = &(ipv6->sin6_addr);
-			ipver = "IPv6";
-		}
 
-		// convert the IP to a string and print it:
-		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-		printf("  %s: %s\n", ipver, ipstr);
-	}
+int main() {
 
-	freeaddrinfo(res); // free the linked list
+        //define the server and client file descriptor
 
-	return 0;
+        int server_fd, client_fd;
+
+
+
+        //define the socket address 
+
+        struct sockaddr_in address;
+
+        int address_len = sizeof(address);
+
+
+
+        //define the application buffer where we receive the requests
+
+        // data will be moved from receive buffer to here
+
+        char buffer[APP_MAX_BUFFER] = {0};
+
+
+
+        // Create socket
+
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+
+                perror("Socket failed");
+
+                exit(EXIT_FAILURE);
+
+        }
+
+
+
+        //Bind socket
+
+        address.sin_family = AF_INET; //ipv4
+
+        address.sin_addr.s_addr = INADDR_ANY; //listen 0.0.0.0 interfaces
+
+
+
+
+
+        address.sin_port = htons(PORT);
+
+
+
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+
+                perror("Bind failed");
+exit(EXIT_FAILURE);
+
+        }
+
+
+
+
+
+        // Creates the queues
+
+        // Listen for clients, with 10 backlog (10 connections in accept queue) 
+
+        if (listen (server_fd, 10) < 0) {
+
+                perror("Listen failed");
+
+                exit(EXIT_FAILURE);
+
+        }
+
+
+
+
+
+        //we loop forever 
+
+        while(1) {
+
+
+
+                printf("\n Waiting for a connection....\n");
+
+
+
+                // Accept a client connection client_fd == connection 
+
+                // // this blocks
+
+                if ((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&address_len)) < 0) {
+
+                        perror("Accept failed");
+                        exit(EXIT_FAILURE);
+
+                }
+
+
+
+
+
+                //read data from OS received buffer to the application (buffer)
+
+                // this is essentially reading the HTTP request 
+
+                // don't bite more than you chew APP_MAX_BUFFER
+
+                read(client_fd, buffer, APP_MAX_BUFFER);
+
+                printf("%s\n", buffer);
+
+
+
+
+
+                // We send the request by writing to the socket send buffer in the OS
+
+                char *http_response = "HTTP/1.1 200 OK\n"
+
+                                     "Content-Type: text/plain\n"
+
+                                     "Content-Length: 13\n\n"
+
+                                     "Hello world!\n";
+ 		//write to the socket
+
+                //send queue os 
+
+                write(client_fd, http_response, strlen(http_response));
+
+
+
+                //close the client socket (terminate TCP connection)
+
+                close(client_fd);
+
+        }
+
+        return 0;
+
 }
